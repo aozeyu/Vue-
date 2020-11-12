780704faa5fae770ae8764fb35b0bf46e3bac7d8
e <3484766443@qq.com>
2020-11-12 13:15:57 +0800

    finish city css

--- travel-project/src/pages/city/City.vue
+++ travel-project/src/pages/city/City.vue
@@ -2,19 +2,27 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from "./components/Header";
 import CitySearch from "./components/Search";
+import CityList from "./components/List";
+import CityAlphabet from "./components/Alphabet";
 export default {
   name: "City", //导出City组件
   components: {
     CityHeader,
     CitySearch,
+    CityList,
+    CityAlphabet //别忘了注册组件
   },
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+
+</style>
